fb2dca0723ef1a30c2f30c66ce74ebf4af81494c
Thomas Gurr <tpgurr21@gmail.com>
2024-01-28 23:56:52 -0500

    mouseenter event listener

--- script.js
+++ script.js
@@ -170,10 +170,13 @@ btnScrollTo.addEventListener('click', function (e) {
 
 const h1 = document.querySelector('h1');
 
-h1.addEventListener('mouseenter', function(e) {
+const alertH1 = function (e) {
   alert('addEventListener: Great! You are reading the heading :D');
-});
+};
+h1.addEventListener('mouseenter', alertH1);
 
-h1.onmouseenter = function(e) {
-  alert('onmouseenter: Great! You are reading the heading :D');
-}
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function(e) {
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// }

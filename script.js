91bc85e4fdbce56369103674c49f85f20155c7da
Thomas Gurr <tpgurr21@gmail.com>
2024-01-30 21:18:25 -0500

    Made buttons scroll smoothly

--- script.js
+++ script.js
@@ -66,13 +66,26 @@ btnScrollTo.addEventListener('click', function (e) {
 
 // Page navigation
 
-document.querySelectorAll('.nav__link').forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    e.preventDefault();
-    const id = this.getAttribute('href');
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
     console.log(id);
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  });
+  }
 });
 
 /////////////////////////////////////
@@ -223,3 +236,4 @@ document.querySelector('.nav').addEventListener(
 );
 
 */
+

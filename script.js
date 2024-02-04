6742514e55dbaca783f289a5512d900522dd58e6
Thomas Gurr <tpgurr21@gmail.com>
2024-02-04 16:01:58 -0500

    starting fade animation

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
 /////////////////////////////////////
 // Modal window
@@ -89,24 +93,39 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 });
 
 // Tabbed component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
 
-// tabs.forEach(t=>t.addEventListener('click', () => console.log('TAB'))) 
-tabsContainer.addEventListener('click', function(e) {
+
+// tabs.forEach(t=>t.addEventListener('click', () => console.log('TAB')))
+tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
-  console.log(clicked);
 
   // Guard clause
-  if(!clicked) return;
+  if (!clicked) return;
 
-  tabs.forEach(t => t.classList.remove('operations__tab--active'))
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
   clicked.classList.add('operations__tab--active');
 
   // Activate content area
-  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
-})
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+// Menu fade animation
+nav.addEventListener('mouseover', function(e) {
+  if(e.target.classList.contains('nav__link')) {
+    
+  }
+} )
+
+nav.addEventListener('mouseout', function(e) {
+
+} )
+
 
 /////////////////////////////////////
 /////////////////////////////////////

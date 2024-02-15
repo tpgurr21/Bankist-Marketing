d1c2fda5ddd6f6702a5ebd351309b45098ee3dc6
Thomas Gurr <tpgurr21@gmail.com>
2024-02-14 21:23:02 -0500

    added class for tabs

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ const handleHover = function (e) {
     });
     logo.style.opacity = this;
   }
-};
+}
 
 // Passing "argument" into handler
 nav.addEventListener('mouseover', handleHover.bind(0.5));

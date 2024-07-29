600a5b9e33e2844ff45a3c648b6a1af7a3a56c54
Thomas Gurr <tpgurr21@gmail.com>
2024-07-29 18:15:34 -0400

    lifecycle DOM events

--- script.js
+++ script.js
@@ -331,7 +331,7 @@ dotContainer.addEventListener('click', function(e) {
   }
 });
 };
-slider();
+slider(7);
 
 
 /////////////////////////////////////
@@ -513,3 +513,17 @@ console.log(h1.parentElement.children);
 });
 
 */
+
+document.addEventListener('DOMContentLoaded', function(e) {
+  console.log('HTML parsed and DOM tree built!', e)
+});
+
+window.addEventListener('load', function(e) {
+  console.log('Page fully loaded', e);
+});
+
+// window.addEventListener('beforeunload', function(e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = ''; // Doesn't work anymore
+// })
